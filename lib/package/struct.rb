d62586801440f29de2b1ae657cfba6d0c4df6309
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2019-08-08 22:32:39 -0400

    Lib: Use Dry::Struct for Package::Struct

--- lib/package/struct.rb
+++ lib/package/struct.rb
@@ -1,97 +1,38 @@
+require 'package/types'
+require 'dry/struct'
+
 module Package
-  class Struct
+  class Struct < Dry::Struct
 
-    Source = ::Struct.new(:url, :path)
+    def self.attribute?(name, type)
+      super
 
-    attr_reader :name
-
-    def name=(value)
-      @name = value.to_sym
+      define_method("#{name}?") do
+        !!instance_variable_get("@#{name}")
+      end
     end
 
-    attr_accessor :version
-    attr_accessor :archive
-    attr_accessor :checksum
-    attr_accessor :signature
-    attr_accessor :files
+    def initialize(*arguments)
+      super
 
-    def archive_basename
-      File.basename(@archive_url)
+      @identifier = [@name, @version].join(?-)
     end
 
-    def files?
-      !!@files
-    end
+    attribute  :name,       Types::Symbol
+    attribute  :version,    Types::String
 
-    def build_files
-      @files.map { |path| paths.build_root.path.join(path.to_s.gsub(/^\//, '')) }
-    end
+    attribute  :archive,    Types::Source
+    attribute? :checksum,   Types::Source
+    attribute? :signature,  Types::Source
 
-    def checksum?
-      !!@checksum
-    end
+    attribute? :files,      Types::StringArray
 
-    def external_checksum?
-      checksum? && @checksum.start_with?('http')
-    end
+    attribute? :on_build,   Types::Callback
+    attribute? :on_check,   Types::Callback
+    attribute? :on_verify,  Types::Callback
+    attribute? :on_package, Types::Callback
 
-    def internal_checksum?
-      !external_checksum?
-    end
-
-    def signature?
-      !!@signature
-    end
-
-    attr_accessor :identifier
-
-    attr_accessor :archive_url
-    attr_accessor :archive_path
-
-    attr_accessor :checksum_url
-    attr_accessor :checksum_path
-
-    attr_accessor :signature_url
-    attr_accessor :signature_path
-
-    attr_accessor :build_path
-
-    attr_accessor :lock_path
-    attr_accessor :checksum_lock_path
-    attr_accessor :signature_lock_path
-    attr_accessor :build_lock_path
-
-    def sources
-      sources = []
-
-      sources << Source.new(archive_url,   archive_path)
-      sources << Source.new(signature_url, signature_path) if signature?
-      sources << Source.new(checksum_url,  checksum_path)  if external_checksum?
-
-      sources
-    end
-
-    def source_paths
-      sources.map(&:path)
-    end
-
-    attr_accessor :on_verify
-
-    def on_verify?
-      !!@on_verify
-    end
-
-    attr_accessor :on_build
-
-    def on_build?
-      !!@on_build
-    end
-
-    attr_accessor :on_package
-
-    def on_package?
-      !!@on_package
-    end
+    attr_reader :identifier
 
   end
 end
