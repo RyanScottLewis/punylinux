73d6455db523970e26b9047ea7ad06fa2b63487d
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2019-08-08 22:34:45 -0400

    Lib: Package loading methods, load in phases - into spec, then into the struct; to allow for validation

--- lib/package.rb
+++ lib/package.rb
@@ -1,5 +1,10 @@
+require 'yaml'
+require 'json'
 require 'package/dsl'
 require 'package/list'
+require 'package/specification'
+require 'package/load_result'
+require 'package/load_results'
 
 module Package
   class << self
@@ -8,8 +13,60 @@ module Package
       @all ||= List.new
     end
 
-    def define(&block)
-      all << DSL.call(&block)
+    def load_ruby(path)
+      load_file(path) do
+        DSL.load(path)
+      end
+    end
+
+    def load_yaml(path)
+      load_file(path) do
+        YAML.load_file(path)
+      end
+    end
+
+    def load_json(path)
+      load_file(path) do
+        data = File.read(path) # TODO: JSON.load_file ffs...
+        JSON.load(data)
+      end
+    end
+
+    def load_directory(directory)
+      load_results = find_by_ext(directory, '{rb,yml,yaml,json}').map do |path|
+        case File.extname(path).downcase[1..-1]
+        when 'rb'          then load_ruby(path)
+        when 'yml', 'yaml' then load_yaml(path)
+        when 'json'        then load_json(path)
+        end
+      end
+
+      LoadResults.new(load_results)
+    end
+
+    def load_all(directory)
+      load_results = load_directory(directory)
+
+      all.push(*load_results.to_structs)
+
+      load_results
+    end
+
+    protected
+
+    def find_by_ext(directory, extension)
+      pattern = File.join(directory, '**', "*.#{extension}")
+
+      Dir.glob(pattern, File::FNM_CASEFOLD) # FNM_CASEFOLD = case-insensitive
+    end
+
+    def load_file(path)
+      data          = yield
+      specification = Specification.call(data)
+
+      LoadResult.new(path, specification, nil)
+    rescue => error
+      LoadResult.new(path, nil, error)
     end
 
   end
