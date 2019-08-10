5a0654f697b28735c91563a180627f60aa4e54dc
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2019-08-10 06:34:39 -0400

    Lib: Print paths based on whether showing all or not

--- lib/path/printer.rb
+++ lib/path/printer.rb
@@ -9,11 +9,28 @@ module Path
       value_justification = paths.value_justification
 
       paths.each do |path|
-        puts "%s = %s # %s" % [
-          path.name.to_s.ljust(name_justification),
-          path.value.to_s.ljust(value_justification),
-          path.description
-        ]
+        message = if all
+          if path.description?
+            "# %s\n%s = %s\n\n" % [
+              path.description,
+              path.name.to_s.ljust(name_justification),
+              path.value
+            ]
+          else
+            "%s = %s\n\n" % [
+              path.name.to_s.ljust(name_justification),
+              path.value
+            ]
+          end
+        else
+          "%s = %s # %s" % [
+            path.name.to_s.ljust(name_justification),
+            path.value.to_s.ljust(value_justification),
+            path.description
+          ]
+        end
+
+        puts message
       end
     end
 
