171ae560907d7969a5c9714c80e589aeb50df55a
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2019-08-13 07:11:23 -0400

    Lib: Fix make helper env

--- lib/helpers/build.rb
+++ lib/helpers/build.rb
@@ -2,7 +2,7 @@ module Helpers
   module Build
 
     def make(task=nil, env={})
-      env = env.map { |k, v| [k, v].join(?=) }.join(' ')
+      env = env.map { |k, v| [k, v].join(?=) }
 
       command = ['make'] + env
       command << (task || "-j`nproc`")
